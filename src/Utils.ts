c1f953db1e0bddfe8ef1b6f913c7aa67ab5b2c74
Pablo-Genial <sporthold@yahoo.com>
2021-01-22 18:56:20 +0100

    static

--- src/Utils.ts
+++ src/Utils.ts
@@ -46,7 +46,7 @@ class Utils extends Debouncer {
      * @param el {Element} - egy DOM Element típus
      * @returns {void}
      */
-    removeNode(el: Element): void {
+    static removeNode(el: Element): void {
         el.parentNode.removeChild(el);
     }
 }
